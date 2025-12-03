5868b1394189c37eda872b1dc0c19104629cd3f1
Nimesh04 <nimeshb907@gmail.com>
2025-12-03 08:03:17 -0600

    API query & Display of the search result on DOM
    API call now includes dynamic location & displays
    the result of the current day & the next 15 days
    as well.

--- src/index.js
+++ src/index.js
@@ -5,25 +5,78 @@ const weatherData = document.querySelector("#weather-data");
 const searchBtn = document.querySelector("#searchBtn");
 
 searchBtn.addEventListener("click", () =>{
-    console.log("clicked")
     const searchInput = document.querySelector("#search");
 
-    getWeatherData();
+    getWeatherData(searchInput.value);
 })
 
 
 
-async function getWeatherData(){
+async function getWeatherData(searchInput){
     const params = new URLSearchParams();
+    params.append('location', searchInput);
     params.append('unitGroup','us');
     params.append('key','EM8A3GWWB7J9N3ADDKYG6U4JE');
 
-    const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/mankato?';
-    const data = fetch(url + params.toString())
-                    .then(response => response.json())
-                    .then(console.log);
-
+    const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/?';
+    const data = await fetch(url + params.toString())
+                    .then(function(response){ 
+                        return response.json()
+                    });
+    
+    console.log(data);
+    updateHtml(data);
 
 
 }
 
+function updateHtml(data){
+    // const weatherDiv  = document.querySelector("#weather-data");
+    const futureForecast = document.querySelector(".future-forecast");
+    const weatherAddress = document.querySelector(".weather-address");
+
+
+    weatherAddress.textContent = data.address;
+
+    const currentTemp = document.querySelector(".current-temp");
+
+    displayCurrentTemp(currentTemp, data);
+
+    let next15 = data.days;
+    
+    next15.forEach(elm =>{
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("futureElm");
+        displayFutureTemp(newDiv, elm);
+        futureForecast.append(newDiv);
+        
+    });
+
+
+}
+
+
+function displayCurrentTemp(elm, data){
+    elm.innerHTML = ``;
+    elm.innerHTML =`
+    <p>Feels like: ${data.currentConditions.feelslike}</p>
+    <p>Humidity: ${data.currentConditions.humidity}</p>
+    <p>Snow: ${data.currentConditions.snow}</p>
+    <p>Sunrise: ${data.currentConditions.sunrise}</p>
+    <p>Sunset: ${data.currentConditions.sunset}</p>
+    <p>Wind Speed: ${data.currentConditions.windspeed}</p>`;
+
+}
+
+function displayFutureTemp(elm, data){
+    elm.innerHTML = ``;
+    elm.innerHTML =`
+    <p>Date: ${data.datetime}</p>
+    <p>Feels like: ${data.feelslike}</p>
+    <p>Humidity: ${data.humidity}</p>
+    <p>Snow: ${data.snow}</p>
+    <p>Sunrise: ${data.sunrise}</p>
+    <p>Sunset: ${data.sunset}</p>
+    <p>Wind Speed: ${data.windspeed}</p>`;
+
+}
